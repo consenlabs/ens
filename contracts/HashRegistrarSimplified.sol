26028a131b2a259587a7bd2b2b95f0870f378ad1
Caizhen Liu <czliu@Caizhens-MacBook-Air.local>
2017-04-21 11:09:25 +0800

    format registrar contract

--- contracts/HashRegistrarSimplified.sol
+++ contracts/HashRegistrarSimplified.sol
@@ -86,7 +86,7 @@ contract Deed {
      */
     function destroyDeed() {
         if (active) throw;
-        
+
         // Instead of selfdestruct(owner), invoke owner fallback function to allow
         // owner to log an event if desired; but owner should also be aware that
         // its fallback function can also be invoked by setBalance
